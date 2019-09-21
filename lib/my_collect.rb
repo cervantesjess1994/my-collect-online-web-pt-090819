ac29fd19f444ef5a0b003756edca2352c6c45a03
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-21 05:38:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
+def collect(array)
+  i = 0
+  while i < array.length
+    yield(array[1])
+    i += 1
+  end
+  collect
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
